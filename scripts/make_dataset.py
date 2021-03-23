944706dfc538ae5ac3a3d8304813a0fb5566248a
Lianmin Zheng <lianminzheng@gmail.com>
2021-03-23 11:45:02 +0000

    update make_dataset.py

--- scripts/make_dataset.py
+++ scripts/make_dataset.py
@@ -1,28 +1,33 @@
 """Make a dataset file.
 
 Usage:
-python3 make_dataset.py 1060.json v100.json
-python3 make_dataset.py bert-B1-llvm.json --load-network
+python3 make_dataset.py dataset/measure_records/e5-2666/*.json
+python3 make_dataset.py dataset/measure_records/e5-2666/*.json --max-in-files 10
 """
 import argparse
 import glob
+import random
 
 from tvm import auto_scheduler
 
-import common
-from measure_programs import load_and_register_tasks
+from common import load_and_register_tasks
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("logs", nargs="+", type=str)
+    parser.add_argument("--sample-in-files", type=int)
+    parser.add_argument("--seed", type=int, default=0)
     parser.add_argument("--out-file", type=str, default='dataset.pkl')
     parser.add_argument("--min-sample-size", type=int, default=48)
     args = parser.parse_args()
 
     load_and_register_tasks()
 
-    log_files = glob.glob("dataset/measure_records/e5-2673/*.json")
+    files = args.logs
+    if args.sample_in_files:
+        random.seed(args.seed)
+        files = random.sample(files, args.sample_in_files)
 
     auto_scheduler.dataset.make_dataset_from_log_file(
-        log_files, args.out_file, args.min_sample_size)
-
+        files, args.out_file, args.min_sample_size)
