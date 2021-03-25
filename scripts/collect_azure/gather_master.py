f512f5f9aaf61712bb0b3f478e542e6853dbc0d7
Lianmin Zheng <lianminzheng@gmail.com>
2021-03-25 15:00:44 -0700

    fix gather_master for azure

--- scripts/collect_azure/gather_master.py
+++ scripts/collect_azure/gather_master.py
@@ -8,7 +8,7 @@ from collect_master import run_cmd, ssh_run, n_machines
 if __name__ == "__main__":
     for i in tqdm(range(n_machines)):
         print(i)
-        host_name = f"c44X_{i:02d}"
+        host_name = f"azure-intel-avx512-{i:02d}"
 
         run_cmd(f"scp -o StrictHostKeyChecking=no {host_name}:tvm-cost-model/scripts/dataset_part_*.zip .")
 
