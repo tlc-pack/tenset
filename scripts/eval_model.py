261510fe38eceed9f2aac9be734ddb62ccff1385
ruochen99 <ruochen99@berkeley.edu>
2021-04-11 04:58:19 +0000

    mlp model in eval

--- scripts/eval_model.py
+++ scripts/eval_model.py
@@ -9,7 +9,7 @@ import tvm
 from tvm import auto_scheduler
 from tvm.auto_scheduler.dataset import LearningTask
 from tvm.auto_scheduler.cost_model.xgb_model import XGBModelInternal
-
+from tvm.auto_scheduler.cost_model.mlp_model import MLPModelInternal
 from common import get_task_info_filename, get_measure_record_filename
 
 
@@ -75,12 +75,14 @@ if __name__ == "__main__":
     network_keys = [
         ("resnet_50", [(1, 3, 224,224)]),
         ("mobilenet_v2", [(1, 3, 224,224)]),
-        ("mobilenet_v3", [(1, 3, 224,224)]),
+        ("resnext_50", [(1, 3, 224,224)]),
         ("bert_base", [(1, 128)]),
+        ("bert_tiny", [(1, 128)]),
     ]
     target = "llvm -model=platinum-8272"
 
-    model = XGBModelInternal()
+    #model = XGBModelInternal()
+    model = MLPModelInternal()
     model.load(model_file)
 
     top_ks = [1, 5]
