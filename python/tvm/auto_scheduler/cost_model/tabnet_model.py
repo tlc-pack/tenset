f928e43f57030af2ef7ae59b1a175c7d8b09b2fd
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2021-06-29 17:04:49 +0800

    Update tabnet_model.py

--- python/tvm/auto_scheduler/cost_model/tabnet_model.py
+++ python/tvm/auto_scheduler/cost_model/tabnet_model.py
@@ -35,6 +35,187 @@ from torch.nn import Linear, BatchNorm1d, ReLU
 import numpy as np
 from .sparsemax import sparsemax
 
+class AttentiveTransformer(torch.nn.Module):
+    def __init__(
+        self,
+        input_dim,
+        output_dim,
+        virtual_batch_size=128,
+        momentum=0.02,
+        mask_type="sparsemax",
+    ):
+        """
+        Initialize an attention transformer.
+        Parameters
+        ----------
+        input_dim : int
+            Input size
+        output_dim : int
+            Output_size
+        virtual_batch_size : int
+            Batch size for Ghost Batch Normalization
+        momentum : float
+            Float value between 0 and 1 which will be used for momentum in batch norm
+        mask_type : str
+            Either "sparsemax" or "entmax" : this is the masking function to use
+        """
+        super(AttentiveTransformer, self).__init__()
+        self.fc = Linear(input_dim, output_dim, bias=False)
+        initialize_non_glu(self.fc, input_dim, output_dim)
+        self.bn = GBN(
+            output_dim, virtual_batch_size=virtual_batch_size, momentum=momentum
+        )
+
+        if mask_type == "sparsemax":
+            # Sparsemax
+            self.selector = sparsemax.Sparsemax(dim=-1)
+        elif mask_type == "entmax":
+            # Entmax
+            self.selector = sparsemax.Entmax15(dim=-1)
+        else:
+            raise NotImplementedError(
+                "Please choose either sparsemax" + "or entmax as masktype"
+            )
+
+    def forward(self, priors, processed_feat):
+        x = self.fc(processed_feat)
+        x = self.bn(x)
+        x = torch.mul(x, priors)
+        x = self.selector(x)
+        return x
+
+
+class FeatTransformer(torch.nn.Module):
+    def __init__(
+        self,
+        input_dim,
+        output_dim,
+        shared_layers,
+        n_glu_independent,
+        virtual_batch_size=128,
+        momentum=0.02,
+    ):
+        super(FeatTransformer, self).__init__()
+        """
+        Initialize a feature transformer.
+        Parameters
+        ----------
+        input_dim : int
+            Input size
+        output_dim : int
+            Output_size
+        shared_layers : torch.nn.ModuleList
+            The shared block that should be common to every step
+        n_glu_independent : int
+            Number of independent GLU layers
+        virtual_batch_size : int
+            Batch size for Ghost Batch Normalization within GLU block(s)
+        momentum : float
+            Float value between 0 and 1 which will be used for momentum in batch norm
+        """
+
+        params = {
+            "n_glu": n_glu_independent,
+            "virtual_batch_size": virtual_batch_size,
+            "momentum": momentum,
+        }
+
+        if shared_layers is None:
+            # no shared layers
+            self.shared = torch.nn.Identity()
+            is_first = True
+        else:
+            self.shared = GLU_Block(
+                input_dim,
+                output_dim,
+                first=True,
+                shared_layers=shared_layers,
+                n_glu=len(shared_layers),
+                virtual_batch_size=virtual_batch_size,
+                momentum=momentum,
+            )
+            is_first = False
+
+        if n_glu_independent == 0:
+            # no independent layers
+            self.specifics = torch.nn.Identity()
+        else:
+            spec_input_dim = input_dim if is_first else output_dim
+            self.specifics = GLU_Block(
+                spec_input_dim, output_dim, first=is_first, **params
+            )
+
+    def forward(self, x):
+        x = self.shared(x)
+        x = self.specifics(x)
+        return x
+
+
+class GLU_Block(torch.nn.Module):
+    """
+    Independent GLU block, specific to each step
+    """
+
+    def __init__(
+        self,
+        input_dim,
+        output_dim,
+        n_glu=2,
+        first=False,
+        shared_layers=None,
+        virtual_batch_size=128,
+        momentum=0.02,
+    ):
+        super(GLU_Block, self).__init__()
+        self.first = first
+        self.shared_layers = shared_layers
+        self.n_glu = n_glu
+        self.glu_layers = torch.nn.ModuleList()
+
+        params = {"virtual_batch_size": virtual_batch_size, "momentum": momentum}
+
+        fc = shared_layers[0] if shared_layers else None
+        self.glu_layers.append(GLU_Layer(input_dim, output_dim, fc=fc, **params))
+        for glu_id in range(1, self.n_glu):
+            fc = shared_layers[glu_id] if shared_layers else None
+            self.glu_layers.append(GLU_Layer(output_dim, output_dim, fc=fc, **params))
+
+    def forward(self, x):
+        scale = torch.sqrt(torch.FloatTensor([0.5]).to(x.device))
+        if self.first:  # the first layer of the block has no scale multiplication
+            x = self.glu_layers[0](x)
+            layers_left = range(1, self.n_glu)
+        else:
+            layers_left = range(self.n_glu)
+
+        for glu_id in layers_left:
+            x = torch.add(x, self.glu_layers[glu_id](x))
+            x = x * scale
+        return x
+
+
+class GLU_Layer(torch.nn.Module):
+    def __init__(
+        self, input_dim, output_dim, fc=None, virtual_batch_size=128, momentum=0.02
+    ):
+        super(GLU_Layer, self).__init__()
+
+        self.output_dim = output_dim
+        if fc:
+            self.fc = fc
+        else:
+            self.fc = Linear(input_dim, 2 * output_dim, bias=False)
+        initialize_glu(self.fc, input_dim, 2 * output_dim)
+
+        self.bn = GBN(
+            2 * output_dim, virtual_batch_size=virtual_batch_size, momentum=momentum
+        )
+
+    def forward(self, x):
+        x = self.fc(x)
+        x = self.bn(x)
+        out = torch.mul(x[:, : self.output_dim], torch.sigmoid(x[:, self.output_dim :]))
+        return out
 
 def initialize_non_glu(module, input_dim, output_dim):
     gain_value = np.sqrt((input_dim + output_dim) / np.sqrt(4 * input_dim))
