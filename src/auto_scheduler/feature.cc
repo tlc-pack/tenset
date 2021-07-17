63d13cce9fdbc4263e1e90c5b5b0f506afb40477
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2021-07-18 01:06:12 +0800

    Update feature.cc

--- src/auto_scheduler/feature.cc
+++ src/auto_scheduler/feature.cc
@@ -1418,7 +1418,7 @@ void GetPerStoreFeaturesFromStates(const Array<State>& states, const SearchTask&
 
   std::atomic<int> error_ct(0);
 
-  for (int i = 0; i < 500; i++) {
+  for (int i = 0; i < 5; i++) {
     GetPerStoreFeaturesWorkerFunc(task, states[i], max_n_bufs,
                                                         &(*features)[i], &error_ct);
   }
@@ -1438,7 +1438,7 @@ void GetPerStoreFeaturesFromStates(const Array<State>& states, const std::vector
 
   std::atomic<int> error_ct(0);
 
-  for (int i = 0; i < 500; i++) {
+  for (int i = 0; i < 5; i++) {
     GetPerStoreFeaturesWorkerFunc(tasks[i], states[i], max_n_bufs,
                                                         &(*features)[i], &error_ct);
   }
