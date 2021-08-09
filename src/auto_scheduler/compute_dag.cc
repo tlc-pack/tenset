1972b7f290d688886789d7e108ad90fd080fff4f
Peiyuan Liao <34192321+liaopeiyuan@users.noreply.github.com>
2021-08-09 17:48:12 +0800

    Update compute_dag.cc

--- src/auto_scheduler/compute_dag.cc
+++ src/auto_scheduler/compute_dag.cc
@@ -1303,8 +1303,8 @@ String ComputeDAG::ComputeAccessMatrix(bool simple_mode) const {
             PrimExpr rhs = pop->body[k];
             if (rhs->IsInstance<AddNode>()) {
               const auto& ad = rhs.as<AddNode>();
-              ss << ad->a->IsInstance<CallNode>();
-              ss << ad->b->IsInstance<CallNode>();
+              ss << ad->a->IsInstance<BufferLoadNode>();
+              ss << ad->b->IsInstance<BufferLoadNode>();
               ss << " *add* " << ad->a << ad->b ;
             }
             ss << " = " << pop->body[k] << "\n";
