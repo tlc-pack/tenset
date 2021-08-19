ee5f04a7a4668ea32fe735c74bd63731004444db
Peiyuan Liao <peiyuanl@cs.cmu.edu>
2021-08-19 14:22:27 -0600

    integrating buffer feature

--- src/auto_scheduler/graph_feature.cc
+++ src/auto_scheduler/graph_feature.cc
@@ -77,11 +77,71 @@ inline float slog2(float x) {
   return slog(slog(x));
 }
 
+static const int ARITH_INTENSITY_CURVE_SAMPLE_N = 10;
+
+// Feature set of an arbitrary statement
+struct FeatureSet {
+  // Group 1: Computation related features
+  float float_mad;                  // The number of float MAD (Multiply–add) ops
+  float float_addsub;               // The number of float add and sub ops
+  float float_mul;                  // The number of float multiply ops
+  float float_divmod;               // The number of float div and mod ops
+  float float_cmp;                  // The number of float comparison ops
+  float float_math_func;            // The number of float math func calls
+  float float_other_func;           // The number of other float func calls
+  float int_mad;                    // The number of integer MAD (Multiply–add) ops
+  float int_addsub;                 // The number of integer add and sub ops
+  float int_mul;                    // The number of float multiply ops
+  float int_divmod;                 // The number of float div and mod ops
+  float int_cmp;                    // The number of float comparison ops
+  float int_math_func;              // The number of float math func calls
+  float int_other_func;             // The number of other float func calls
+  float bool_op;                    // The number of bool ops
+  float select_op;                  // The number of select ops
+  float vec_num;                    // The number of vectorized iterators
+  float vec_prod;                   // The product of the lengths of vectorized iterators
+  float vec_len;                    // The length of the innermost vectorized iterator
+  AnnotationPosType vec_type;       // The type of vectorization position
+  float unroll_num;                 // The number of unrolled iterators
+  float unroll_prod;                // The product of the lengths of vectorized iterators
+  float unroll_len;                 // The length of the innermost unrolled iterator
+  AnnotationPosType unroll_type;    // The type of unroll position
+  float parallel_num;               // The number of paralleled iterators
+  float parallel_prod;              // The product of the lengths of paralleled iterators
+  float parallel_len;               // The length of the innermost paralleled iterators
+  AnnotationPosType parallel_type;  // The type of parallel position
+  float is_gpu;                     // Whether it is a GPU task
+  float blockIdx_x_len;             // The length of blockIdx.x
+  float blockIdx_y_len;             // The length of blockIdx.y
+  float blockIdx_z_len;             // The length of blockIdx.z
+  float threadIdx_x_len;            // The length of threadIdx.x
+  float threadIdx_y_len;            // The length of threadIdx.y
+  float threadIdx_z_len;            // The length of threadIdx.z
+  float vthread_len;                // The length of virtual thread
+
+  // Group 2: Buffer access related features (per buffer)
+  std::vector<BufferAccessFeature> access_feas;
+
+  // Group 3: Arithmetic intensity related features
+  float arith_intensity_curve[ARITH_INTENSITY_CURVE_SAMPLE_N];  // points sampled from the
+                                                                // arithmetic intensity curve
+
+  // Group 4: Allocation related features
+  float alloc_size;        // The size of allocated buffer in bytes
+  float alloc_outer_prod;  // The product of lengths of loops outside the scope of the allocation
+  float alloc_inner_prod;  // The product of lengths of loops inside the score of the allocation
+  float alloc_prod;        // alloc_outer_prod * alloc_inner_prod
+
+  // Group 5: Outer scope related features
+  float outer_prod;            // The product of lengths of outer loops
+  float num_loops;             // The number of outer loops
+  float auto_unroll_max_step;  // The value of pragma "auto_unroll_max_step"
+};
+
 
 class NodeGather : public StmtExprVisitor {
  public:
   void VisitStmt(const Stmt& n) {
-//    int count = static_cast<int>(node_to_index.size());
     int primExprType = 0;
     int64_t forExtent = 0;
     int64_t forMin = 0;
