c93dde218e1c583d89ec95fbac54f592e5591d75
Peiyuan Liao <peiyuanl@cs.cmu.edu>
2021-08-31 15:57:56 -0400

    Update graph_model.py

--- python/tvm/auto_scheduler/cost_model/graph_model.py
+++ python/tvm/auto_scheduler/cost_model/graph_model.py
@@ -149,7 +149,7 @@ class GraphModel(PythonBasedModel):
 
     def __init__(self):
         self.params = {
-            'batch_size': 128 + 32,
+            'batch_size': 256,
             'itr_num': 500,
             'lr':  0.01,
             'hidden_dim': 64,
