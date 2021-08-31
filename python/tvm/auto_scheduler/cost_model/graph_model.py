d528cc305df5f2986ed1597682f2d0049c08e5c1
Peiyuan Liao <peiyuanl@cs.cmu.edu>
2021-08-31 10:46:34 -0400

    Update graph_model.py

--- python/tvm/auto_scheduler/cost_model/graph_model.py
+++ python/tvm/auto_scheduler/cost_model/graph_model.py
@@ -213,7 +213,7 @@ class GraphModel(PythonBasedModel):
                 opt.zero_grad()
                 prediction = self.GNN(train_batched_graphs[i])
                 #loss = torch.sqrt(loss_func(prediction, train_batched_labels[i].unsqueeze(1).cuda())) #loss_func(prediction, torch.log(train_batched_labels[i].unsqueeze(1).cuda()))
-                loss = self.loss_func(prediction, train_batched_labels[i].cuda())
+                loss = self.loss_func(prediction, train_batched_labels[i].unsqueeze(1).cuda())
                 total_loss += loss.detach().item() * self.params['batch_size']
                 loss.backward()
                 torch.nn.utils.clip_grad_norm_(self.GNN.parameters(), 10)
